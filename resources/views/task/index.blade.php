d23a1feec2442df81bf1311efa69ceeef225c4cd
Eduardo Machaca <eamachaca@icloud.com>
2022-05-30 03:21:19 -0400

    Commit previous to sleep

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some example text. Some example text.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+</div>
+@endsection
