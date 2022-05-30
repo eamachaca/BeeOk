c2966bf55902632637d666df85a6def947983c07
Eduardo Machaca <eamachaca@icloud.com>
2022-05-30 10:30:07 -0400

    Commit to continue development

--- database/migrations/2022_05_30_071627_create_tasks_table.php
+++ database/migrations/2022_05_30_071627_create_tasks_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
